f14d43912e87099fda43061098d86421b53f85a7
balendra <balendra.singh@snapdeal.com>
2015-09-05 03:30:31 +0530

    Updated

--- MeetupServer/src/main/java/com/test/bhuvan/account/UserService.java
+++ MeetupServer/src/main/java/com/test/bhuvan/account/UserService.java
@@ -18,13 +18,7 @@ public class UserService implements UserDetailsService {
 	
 	@PostConstruct
 	protected void initialize() {
-<<<<<<< Updated upstream
-	//	accountRepository.save(new Account("user", "demo", "ROLE_USER"));
-	//	accountRepository.save(new Account("admin", "admin", "ROLE_ADMIN"));
-=======
-//		accountRepository.save(new Account("user", "demo", "ROLE_USER"));
-//		accountRepository.save(new Account("admin", "admin", "ROLE_ADMIN"));
->>>>>>> Stashed changes
+
 	}
 	
 	@Override
